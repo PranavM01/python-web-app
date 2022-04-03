7f3debc8f8b675ebc17d2383ba8f1712530533a1
Manoj Kumar Maurya <manojk.maurya@gmail.com>
2022-04-03 19:47:39 +0530

    new

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ sudo apt -y install python3.9
 sudo apt -y install python3-pip
 pip3 install --upgrade pip
 pip3 install virtualenv
-virtualenv -p /usr/bin/python3.9 venv
+$HOME/.local/bin/virtualenv -p /usr/bin/python3.9 venv
 . venv/bin/activate
 pip3 install -r requirements.txt
 kill $(ps -ef | grep /app/app.py | grep -v grep | awk '{print $2}') || true
