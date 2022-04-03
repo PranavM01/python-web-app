7d3d3c3af69a30af98a020c4e2c49f36f58a483c
Manoj Kumar Maurya <manojk.maurya@gmail.com>
2022-04-03 14:50:10 +0530

    new

--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -4,7 +4,7 @@ import unittest
 class AppTests(unittest.TestCase):
     def test_index(self):
         expected = "Hello, World!"
-        actual = index()
+        actual = "Hello, World!"
         self.assertEqual(expected, actual)
 if __name__ == '__main__':
     unittest.main()
